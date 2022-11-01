23a50e82fab3a9324f66acf18f4dde1a4011d644
Syahrul0212 <syhrl44@gmail.com>
2022-11-01 12:09:09 +0700

    ci4

---
+++ app/Controllers/Chart.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controllers;
+
+//memanggil model
+use App\Models\BukuModel;
+
+class Chart extends BaseController
+{
+    public function __construct()
+    {
+        //load model untuk digunakan
+        $this->BukuModel = new BukuModel();
+    }
+
+    public function pie()
+    {
+        //select data from table buku
+        $list = $this->BukuModel->select('kategori.nama, SUM(buku.stok) AS stok')->join('kategori','buku.kategori_id = kategori.id')->groupBy('nama')->orderBy('nama')->findAll();
+
+        $output = [
+            'list' => $list,
+        ];
+
+        return view('chart_pie', $output);
+    }
+    public function line()
+    {
+        //select data from table buku
+        $list = $this->BukuModel->select('judul, stok')->orderBy('judul')->findAll();
+
+        $output = [
+            'list' => $list,
+        ];
+
+        return view('chart_line', $output);
+    }
+}
